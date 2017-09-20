a1d95af61907abef801c33db52f1240853c605b4
Alexandra.von.Trotha <Alexandra.von.Trotha@SK-D43D7EB3BCCB.2631.skool.net>
2017-09-20 11:37:15 +0200

    Collatz Problem schöner + mit Kommentaren

--- src/CollatzProblem.java
+++ src/CollatzProblem.java
@@ -2,20 +2,32 @@
 public class CollatzProblem {
 
 	public static void main(String[] args) {
+		
+		int n;	//Variable n
 	
-	int n;	
-	for(int i=1; i<=1000; i++)	{
-	n=i;
-	System.out.println(n+":");
-	do{	
-	if(n%2>0){
-		n = 3*n+1;}
-	else{
-		n = n/2;}
-  System.out.print(n+"; ");
+		for(int i=1; i<=1000; i++)	{//Collatz Vermutung für alle Zahlen von 1 bis 1000
+	
+			n=i;
+	
+			System.out.println(n+":");//Anfangswert wird ausgegeben
+	
+			do{	
+	
+				if(n%2>0){ //           wenn Zahl ungerde n*3+1
+					n = 3*n+1;}
+	
+				else{            //wenn Zahl gerade n/2
+						n = n/2;}
+  
+				System.out.print(n+"; ");//Ergebnis ausgeben (kein Zeilenumbruch nach jeder Zahl) 
+				}
+	
+			while(n>1); //Weiterführen bis n=1
+	
+			System.out.println();//Zeilenumbruch
+	
+		}
+
 	}
-	while(n>1);
-	System.out.println();
-	}
-}
+
 }
